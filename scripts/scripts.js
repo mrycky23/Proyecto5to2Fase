2e92492532b88f8c4157875102c58cfc31136a0f
mrycky23 <rycky_jvm@outlook.com>
2024-03-07 12:45:23 -0500

    vistas

---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+function loadView(viewUrl, pageTitle) {
+  fetch(viewUrl)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.text();
+      })
+      .then(data => {
+          document.getElementById("content").innerHTML = data;
+          // Cambiar la URL en la barra de direcciones
+          history.pushState({ page: pageTitle }, pageTitle, viewUrl);
+      })
+      .catch(error => {
+          console.error('There has been a problem with your fetch operation:', error);
+      });
+}
+
+  
+  
