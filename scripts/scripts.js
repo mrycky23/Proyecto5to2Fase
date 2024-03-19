d14f2a661e94859f2a0c70b5e075a2cd7b063043
mrycky23 <rycky_jvm@outlook.com>
2024-03-19 16:03:01 -0500

    script

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,17 @@
 function loadView(viewUrl, pageTitle) {
-  if (viewUrl.startsWith('.')) {
-    viewUrl = viewUrl.substring(1);
-  }
-
   fetch(viewUrl)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.text();
-    })
-    .then((data) => {
-      document.getElementById('content').innerHTML = data;
-      // Cambiar la URL en la barra de direcciones
-      history.pushState({ page: pageTitle }, pageTitle, viewUrl);
-    })
-    .catch((error) => {
-      console.error(
-        'There has been a problem with your fetch operation:',
-        error
-      );
-    });
+      .then(response => {
+          if (!response.ok) {
+              throw new Error('Network response was not ok');
+          }
+          return response.text();
+      })
+      .then(data => {
+          document.getElementById("content").innerHTML = data;
+          // Cambiar la URL en la barra de direcciones
+          history.pushState({ page: pageTitle }, pageTitle, viewUrl);
+      })
+      .catch(error => {
+          console.error('There has been a problem with your fetch operations:', error);
+      });
 }
