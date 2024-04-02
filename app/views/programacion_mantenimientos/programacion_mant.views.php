b9685abb510e091ff3d888ffe9f17e6676edea34
CarBe03 <carolopintag2003@hotmail.com>
2024-04-02 12:40:29 -0500

    dashboard

--- app/views/programacion_mantenimientos/programacion_mant.views.php
+++ app/views/programacion_mantenimientos/programacion_mant.views.php
@@ -38,7 +38,7 @@
                                     <div class="page-breadcrumb">
                                         <nav aria-label="breadcrumb">
                                             <ol class="breadcrumb">
-                                                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Dashboard</a></li>
+                                                <li class="breadcrumb-item"><a href="#" class="breadcrumb-link">Home</a></li>
                                                 <li class="breadcrumb-item active" aria-current="page">Programacion de mantenimientos</li>
                                             </ol>
                                         </nav>
