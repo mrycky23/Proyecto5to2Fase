f901f01aef970b3269572df825b3b62be9270481
CarBe03 <carolopintag2003@hotmail.com>
2024-07-25 18:06:08 -0500

    mensaje de error

--- config/conexion.php
+++ config/conexion.php
@@ -14,7 +14,7 @@ class ClaseConectar
         mysqli_query($this->conexion, "SET NAMES utf8");
         if ($this->conexion === false) die("Error al conectarse con mysql " . mysqli_connect_error());
         $this->db = mysqli_select_db($this->conexion, $this->base);
-        if ($this->db == 0) die("Error conexión con la base de datos " . mysqli_error($this->conexion));
+        if ($this->db == 0) die("Error al momento de la conexión con la base de datos " . mysqli_error($this->conexion));
         return $this->conexion;
     }
 
